da2d3990fe08ab87064411b3e0d9a03d2fd72c39
XIANGBLQ <77910046+XIANGBLQ@users.noreply.github.com>
2021-01-25 20:40:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <!--BEGIN HEAD MATE-->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="initial-scale=1.0,width=device-width"/>
-    <meta name="keywords" content="ZHIHUA·WEI,个人简历主页,多版本简历">
+    <meta name="keywords" content="XUNIY·ZHAO,个人简历主页,多版本简历">
     <meta name="description"
-          content="非你莫属_ZHIHUA.WEI个人简历主页。本项目用于参与码云举办的“非你莫属”为题的Pages简历模板/个性主页征集活动。本项目一共开发了PHP和HTML两种语言版本，前端工程师和后端工程师皆可使用。开源是一种精神！为中国的互联网行业发展献出一份小小的力量。">
+          content="XUNIY·ZHAO个人简历主页。本项目用于参与码云举办的“非你莫属”为题的Pages简历模板/个性主页征集活动。本项目一共开发了PHP和HTML两种语言版本，前端工程师和后端工程师皆可使用。开源是一种精神！为中国的互联网行业发展献出一份小小的力量。">
     <meta name="author" content="ZHIHUA·WEI">
     <meta name="version" content="1.0.0">
     <!--END HEAD MATE-->
@@ -25,7 +25,7 @@
     <!--END HEAD LINK STYLE-->
 
     <!--BEGIN HEAD TITLE-->
-    <title>RESUME-ZHIHUA·WEI</title>
+    <title>RESUME-XUNIY·ZHAO</title>
     <!--END HEAD TITLE-->
 </head>
 <body>
@@ -39,11 +39,11 @@
     <?php
     /**
      * ===============================================
-     * Created by ZHIHUA·WEI.
-     * Author: ZHIHUA·WEI <zhihua_wei@foxmail.com>
+     * Created by XUNIY·ZHAO.
+     * Author: XUNYI·ZHAO <xunyi_zhao@aliyun.com>
      * Date: 2017/12/20 0001
      * Time: Am 10:23
-     * Project: ZHIHUA.WEI Resume
+     * Project: XUNYI·ZHAO Resume
      * Version: 1.0.0
      * Power: Resume Index
      * ===============================================
@@ -64,8 +64,8 @@
 </div>
 <!--END WRAPPER-->
 <!--COPYRIGHT-->
-<div id="copyright">&copy; 2017 - Designed and developed by
-    <a href="http://resume.zhihuawei.xyz/" target="_blank" title="ZHIHUA·WEI">ZHIHUA·WEI</a><br>
+<div id="copyright">&copy; XIANGBLQ - Not forget for a moment
+    <a href="https://xiangblq.github.io/" target="_blank" title="XIANGBLQ">XIANGBLQ</a><br>
     英文版&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="index_ch.php" title="中文版">中文版</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="index.html" title="HTML版">HTML版</a>
 </div>
 <!--END COPYRIGHT-->
